46fe49ef69f4d911989bc5c6f766c85dce7376a3
Elvio Junior <elvio@Elvio-Juniors-MacBook.local>
2010-03-12 17:44:16 -0300

    Organizing duplicated content into adjustable partials

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,9 +2,12 @@ class DashboardsController < ApplicationController
   before_filter :login_required
   before_filter :find_user_stations
   before_filter :auto_follow_profile  
+  before_filter :load_cyloop_recommendations
+  before_filter :load_user_comments
+  
   current_tab :dashboard 
   current_filter :songs
-  layout_except_xhr "base"
+  layout_except_xhr 'base'
   
   def show
     respond_to do |format|
@@ -22,5 +25,15 @@ private
     artist_ids = stations.map{|s| s.station.artist_id}
     Artist.find_all_by_id artist_ids
   rescue
+  end
+  
+  def load_cyloop_recommendations
+    @mixes_recommended = (1..6).to_a
+    @stations_recommended = (1..6).to_a
+    @cyloop_recommends = (1..8).to_a
+  end
+  
+  def load_user_comments
+    @comments = (1..3).to_a
   end
 end
