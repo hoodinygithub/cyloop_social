e17b88f81ad33e323abcfdb72172bad8c9c4dd9b
Elvio Junior <elvio@Elvio-Juniors-MacBook.local>
2010-03-18 19:29:31 -0300

    Adding errors to field bottom and styling the field container

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -9,8 +9,29 @@ Base.hide_success_and_error_messages = function() {
   }
 };
 
+Base.highlight_field_with_errors = function() {
+  if (typeof(field_with_errors) != 'undefined') {
+    for(i=0; i < field_with_errors.length; i++) {
+      
+      field_name = field_with_errors[i][0];
+      field_error = field_with_errors[i][1];
+      
+      field = jQuery('#user_' + field_name);
+      grey_box = field.parent().parent();
+      field.css({'border':'1px solid white'});
+      grey_box.css({'color':'white', 'background':'red', 'border':'2px solid red'});
+      message = jQuery("<br /><span>" +  field_error + "</span>")
+      message.css({'color':'white', 'font-size':'0.8em'});
+      grey_box.append(message);
+    }
+  }
+};
+
 Base.init = function() {
+  jQuery('.png_fix').supersleight({shim: '../images/blank.gif'});
+  jQuery('#slides').cycle({fx: 'fade', timeout: 5000, pager: '#pager_links'});
   this.hide_success_and_error_messages();
+  this.highlight_field_with_errors();
 };
 
 jQuery(document).ready(
