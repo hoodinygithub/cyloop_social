9fc371c5705a10f5addd9c629ed836ef83d3e54d
Alfredo Ribeiro <alfredo.engenharia@gmail.com>
2010-03-19 16:21:23 -0300

    Fixes for Create Station translations Fixes for top radio and recommended radio to fit with new descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,12 +40,21 @@ module ApplicationHelper
     station = station.station if station.is_a? TopStation
     station_link = station.artist
     
-    station_images_with_links = link_to(image_tag(AvatarsHelper.avatar_path(station.artist, :small), :class => 'avatar_four_thumbs'), station_link)
-    station.includes[0..3].each do |artist|
-      station_images_with_links << link_to(image_tag(AvatarsHelper.avatar_path(artist, :small), :class => 'avatar_four_thumbs'), station_link)
+    if options[:type].nil?
+      station_images_with_links = link_to(image_tag(AvatarsHelper.avatar_path(station.artist, :small), :class => 'avatar_four_thumbs'), station_link)
+      station.includes[0..3].each do |artist|
+        station_images_with_links << link_to(image_tag(AvatarsHelper.avatar_path(artist, :small), :class => 'avatar_four_thumbs'), station_link)
+      end
+      station_images_with_links << content_tag(:br, "&nbsp;", :class => 'clearer') if options[:clearer]
+      html = content_tag(:div, station_images_with_links, :class => "four_thubms #{options[:class]}")
+    elsif options[:type] == :big
+      station_images_with_links = link_to(image_tag(AvatarsHelper.avatar_path(station.artist, :small), :class => 'avatar_four_thumbs_big'), station_link)
+      station.includes[0..3].each do |artist|
+        station_images_with_links << link_to(image_tag(AvatarsHelper.avatar_path(artist, :small), :class => 'avatar_four_thumbs_big'), station_link)
+      end
+      station_images_with_links << content_tag(:br, "&nbsp;", :class => 'clearer') if options[:clearer]
+      html = content_tag(:div, station_images_with_links, :class => "four_thubms_big #{options[:class]}")
     end
-    station_images_with_links << content_tag(:br, "&nbsp;", :class => 'clearer') if options[:clearer]
-    html = content_tag(:div, station_images_with_links, :class => "four_thubms #{options[:class]}")
   end
   
 
