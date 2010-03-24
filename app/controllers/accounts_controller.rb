c28b48aaf8f7af02a595ce3912d9797df8491ade
Alfredo Ribeiro <alfredo.engenharia@gmail.com>
2010-03-24 19:00:29 -0300

    User Public - Home - Latest Station - Story ID:2671843

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -17,6 +17,9 @@ class AccountsController < ApplicationController
     @recommended_stations_queue = stations[RECOMMENDED_STATIONS..(stations.size)]
     @top_stations =  profile_account.stations.most_created(3) 
     @followers = profile_account.followers.all(:limit => 4)
+    
+    @latest_stations = profile_account.stations.all(:limit => 6, :order => "stations.created_at DESC")
+
     render :template => 'dashboards/show'
   end
   
