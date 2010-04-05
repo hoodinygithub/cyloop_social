860985d817079ab018408c5dd66b6307d4757c0e
Elvio Junior <elvio@Elvio-Juniors-MacBook.local>
2010-04-05 16:54:14 -0300

    Fixing user top menu to display correct content when user visiting public profile

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,26 +1,44 @@
 module DashboardsHelper
-  def nav_links
-    items = []
+  def nav_links(args = {})
     
-    if page_owner?
-      items.push({:menu => :home,          :label => "#{t('profile.navigation.home')}",             :url => my_dashboard_path })
-      items.push({:menu => :stations,      :label => "#{t('profile.navigation.stations')}",         :url => my_stations_path })
-      items.push({:menu => :settings ,     :label => "#{t('profile.navigation.account_settings')}", :url => my_settings_path })
+    if args[:possessive] or page_owner?
+      links = {
+        :home     => my_dashboard_path, 
+        :stations => my_stations_path, 
+        :settings => my_settings_path,
+        :activities => my_activities_path,
+        :followers  => my_followers_path,
+        :following  => my_following_index_path
+        }
     else
-      items.push({:menu => :home,          :label => "#{t('profile.navigation.home')}",             :url => user_path })
-      items.push({:menu => :stations,      :label => "#{t('profile.navigation.stations')}",         :url => user_stations_path })
+      links = {
+        :home     => user_path, 
+        :stations => user_stations_path, 
+        :settings => nil,
+        :activities => activities_path,
+        :followers  => followers_path,
+        :following  => following_index_path
+        }
     end
     
-    items.push({:menu => :activity,      :label => "#{t('profile.navigation.activity')}",         :url => activities_path })
-    items.push({:menu => :followers,     :label => "#{t('profile.navigation.followers')}",        :url => followers_path })
-    items.push({:menu => :following,     :label => "#{t('profile.navigation.following')}",        :url => following_index_path })
+    items = [
+      {:menu => :home,          :label => "#{t('profile.navigation.home')}",             :url => links[:home]      },
+      {:menu => :stations,      :label => "#{t('profile.navigation.stations')}",         :url => links[:stations]  },
+      {:menu => :activity,      :label => "#{t('profile.navigation.activity')}",         :url => links[:activities]},
+      {:menu => :followers,     :label => "#{t('profile.navigation.followers')}",        :url => links[:followers] },
+      {:menu => :following,     :label => "#{t('profile.navigation.following')}",        :url => links[:following] }
+    ]
 
+    if links[:settings]
+      items.push({:menu => :settings, :label => "#{t('profile.navigation.account_settings')}", :url => links[:settings]}) 
+    end 
+    
     items
   end
   
   
   def user_top_navegation
-    ul_list_to('links', 'current')
+    ul_list_to('links', 'current', true)
   end
   
   
@@ -28,9 +46,9 @@ module DashboardsHelper
     ul_list_to('side_links', 'active')
   end
   
-  def ul_list_to(ul_class, active_class)
+  def ul_list_to(ul_class, active_class, possessive=nil)
     items = []
-    nav_links.each do |item|
+    nav_links(:possessive => possessive).each do |item|
       link_classes = li_classes = ""
       link_classes = active_class if @dashboard_menu == item[:menu]
       li_classes = 'last' if nav_links.last == item
