77e658f6241fa5746fe95336c460a1614786d6ca
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-04-07 21:59:16 -0300

    IE 6 fixes and moving JS methods to base.js

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,5 +1,6 @@
 var Base = {
   message_fadeout_timeout: 8000,
+  listen_icon_timer: null,
   layout: {},
   account_settings: {},
   network: {},
@@ -7,8 +8,23 @@ var Base = {
   header_search: {},
   main_search: {},
   community: {},
-  locale: {}
+  locale: {},
+  utils: {}
 };
+
+/* 
+ * Utils
+ */
+var clearInput = function(value, input) {
+ if(input.value == value) {
+   input.value = '';
+ }
+}
+var restoreInput = function(value, input) {
+ if(input.value == '') {
+   input.value = value;
+ }
+}
 
 /*
  * Layout shared behavior
@@ -19,6 +35,85 @@ Base.layout.hide_success_and_error_messages = function() {
     setTimeout(function() { message.fadeOut(); }, Base.message_fadeout_timeout);
   }
 };
+
+Base.layout.bind_events = function() {
+  //observe userdata arrow button click
+  $('#userdata_arrow').click(function() {
+      $('#userdata_box .links').slideToggle(200);
+      $('#userdata_arrow .arrow_up').toggleClass('visible');
+      
+      return false;
+  });
+  
+  //observe followers setting button click
+  $('.settings_button').click(function() {
+      $('.actions_settings', $(this).closest('.follower_actions')).slideToggle('fast');
+      
+      return false;
+  });
+  
+  //observe .artist_box mouse over
+  $('.artist_box').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+  
+  
+  //observe accordion tabs
+  $('.accordion_title').click(function() {
+      $(this).toggleClass('expanded');
+      $(this).next().slideToggle(200);
+      
+      return false;
+  });
+  
+  //observe listen_icon button mouse over
+  $('.listen_icon').hover(function() {
+    clearTimeout(Base.listen_icon_timer);
+    $('.unsubscribe_mix', $(this).closest('li')).fadeIn(300);
+  }, function() {
+    Base.listen_icon_timer = setTimeout(function() {
+      $('.unsubscribe_mix').fadeOut(300);
+    }, 1000);
+  });
+  
+  $('.unsubscribe_mix').hover(function() {
+    clearTimeout(Base.listen_icon_timer);
+  }, function() {
+    Base.listen_icon_timer = setTimeout(function() {
+      $('.unsubscribe_mix').fadeOut(300);
+    }, 1000);
+  });
+  
+  
+  //observe .songs_box mouse over
+  $('.hoverable_list > li').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+  
+  //observe .rule_tooltip mouse over
+  $('.rule').hover(function() {
+    $t = $('.rule_tooltip', this);
+    $t.css('top', -($t.height()/2));
+    $t.stop();
+    $t.fadeTo(400, 1);
+  }, function() {
+    $t.fadeOut(2000);
+  });
+  $('.rule_tooltip').hover(function() {
+    $(this).stop();
+    $(this).fadeTo(100,1);
+  });
+  
+  //observe popup close button click
+  $('.popup_close, .popup_close_action').click(function() {
+    $(this).closest('.popup').fadeOut('fast');
+    return false;
+  });  
+}
 
 Base.layout.span_button = function(content) {
   return "<span><span>" + content + "</span></span>";
@@ -607,9 +702,11 @@ Base.main_search.select_scope = function() {
  */
 Base.init = function() {
   $('#slides').cycle({fx: 'fade', timeout: 5000, pager: '#pager_links'});
-  this.stations.close_button_event_binder();
-  this.layout.hide_success_and_error_messages();
-  this.header_search.dropdown();
+  $('.png_fix').supersleight({shim: '/images/blank.gif'});  
+  Base.stations.close_button_event_binder();
+  Base.layout.bind_events();  
+  Base.layout.hide_success_and_error_messages();
+  Base.header_search.dropdown();
 };
 
 jQuery(document).ready(
