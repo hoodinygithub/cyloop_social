dd4a017b8946edb070dc21f1db2b1b4701322014
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-04-14 00:26:44 -0300

    Enhancements on User Public and some links

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -74,7 +74,7 @@ module UsersHelper
 
   def possessive(object_key)
     if params[:slug]
-      t("possessives.third_person.#{object_key.to_s}", :subject => profile_account.name)
+      t("possessives.third_person.#{object_key.to_s}", :subject => profile_account.first_name)
     else
       t("possessives.first_person.#{object_key.to_s}")
     end
