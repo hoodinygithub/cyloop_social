9047f9b227dbd003514087bb9b5e86339dc3d787
Jefferson Girao <contato@jefferson.eti.br>
2010-04-15 18:17:48 -0300

    Artist - Album Page - Story ID #2484668

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,22 +1,26 @@
 module AlbumsHelper
 
-  def album_contains(item, limit=3)
+  def album_contains(item, limit=11, show_more = true)
+
+    more_flag = show_more ? item.album_artists.size > limit : false
     album_artists = item.album_artists.limited_to(limit)
+    album_artist_groups = album_artists.in_groups_of(6)
 
     content = ""
+    counter = 1
 
-    album_artists.in_groups_of(2).each do |artists_groups|
-      puts artist_groups
-      artists_groups.each do |album_artist|
-        links << content_tag(:li, link_to(album_artist.artist.name, artist_path(album_artist.artist)))
+    album_artist_groups.each do |album_artist_group|
+      links = []
+      album_artist_group.collect do |album_artist|
+        links << content_tag(:li, link_to(album_artist.artist.name, artist_path(album_artist.artist))) if album_artist
       end
-      content << content_tag(:ol, links) 
+      links << content_tag(:li, t('basics.and_more')) if more_flag && album_artist_group == album_artist_groups.last
+      list  = content_tag(:ol, links, :start => counter)
+      counter += album_artist_group.count
+      content << content_tag(:div, list, :class => "column_wrapper")
     end
 
-    "#{t('basics.contains')}:
-    <div class=\"album_wrapper\">
-      #{content}
-    </div>"
+    "#{content}"
   end
 
 end
