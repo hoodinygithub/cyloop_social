cd88ed1963bf481f9ad911efa2ad7b333a5fb594
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-04-16 15:49:28 -0300

    Set Hoptoad Initializer excepts for development

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,6 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = '950da8268a21bb06642ea9ca5544d41c'
-  config.ignore_user_agent << /(bot)|(spider)/i
+unless Rails.env.development?
+  HoptoadNotifier.configure do |config|
+    config.api_key = '950da8268a21bb06642ea9ca5544d41c'
+    config.ignore_user_agent << /(bot)|(spider)/i
+  end
 end
