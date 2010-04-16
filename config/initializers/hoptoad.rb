62e6cb3d5cc5a12fb1cb57fd0047d726e46fdddf
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-04-16 19:02:47 -0300

    Just use hoptoad on Staging or Production (supressing on UAT)

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
-unless Rails.env.development?
+if Rails.env.staging? || Rails.env.production?
   HoptoadNotifier.configure do |config|
     config.api_key = '950da8268a21bb06642ea9ca5544d41c'
     config.ignore_user_agent << /(bot)|(spider)/i
