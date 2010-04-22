5c60778297f9456fb90d4ac9056318bb46691043
Steven Nedlin <snedlin@hoodiny.com>
2010-04-22 01:10:55 -0400

    Fixes to radio page - double posts, station init.

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -209,7 +209,7 @@ Base.radio.play_station = function(from_list, from_create_station, list, list_pl
 			initCreateStationButton();
 			
 			if(is_owner){
-				Base.radio.refresh_my_stations();
+			  Base.radio.refresh_my_stations();
 			}
 
       swf("cyloop_radio").queueStation(id, queue);
