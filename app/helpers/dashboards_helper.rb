9dfb5e3e1d05581fd5821779acb4a6282bb40465
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-04-30 08:50:58 -0300

    Dashboard - Side menu fixes

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,64 +1,72 @@
 module DashboardsHelper
-  def nav_links(args = {})
+  def artist_nav_links
+    links = {
+      :home     => user_path,
+      :stations => user_stations_path,
+      :activities => activities_path,
+      :albums =>  artist_albums_path,
+      :followers  => followers_path
+    }    
     
-    if args[:possessive] || page_owner?
-      links = {
-        :home     => my_dashboard_path,
-        :stations => my_stations_path,
-        :settings => my_settings_path,
-        :activities => my_activities_path,
-        :followers  => my_followers_path,
-        :following  => my_following_index_path
-        }
-    else
-      links = {
-        :home     => user_path,
-        :stations => user_stations_path,
-        :settings => nil,
-        :activities => activities_path,
-        :albums =>  artist_albums_path,
-        :followers  => followers_path,
-        :following  => following_index_path
-        }
-    end
-
-    items = if profile_artist?
-      [{:menu => :home,          :label => "#{t('profile.navigation.home')}",             :url => links[:home]      },
-       {:menu => :stations,      :label => "#{t('profile.navigation.stations')}",         :url => links[:stations]  },
-       # {:menu => :albums,        :label => "#{t('profile.navigation.albums')}",           :url => links[:albums]},
-       {:menu => :activity,      :label => "#{t('profile.navigation.activity')}",         :url => links[:activities]},
-       {:menu => :followers,     :label => "#{t('profile.navigation.followers')}",        :url => links[:followers] }]
-    else
-      [{:menu => :home,          :label => "#{t('profile.navigation.home')}",             :url => links[:home]      },
-       {:menu => :stations,      :label => "#{t('profile.navigation.stations')}",         :url => links[:stations]  },
-       {:menu => :activity,      :label => "#{t('profile.navigation.activity')}",         :url => links[:activities]},
-       {:menu => :followers,     :label => "#{t('profile.navigation.followers')}",        :url => links[:followers] },
-       {:menu => :following,     :label => "#{t('profile.navigation.following')}",        :url => links[:following] }]
-    end
-
-    if links[:settings]
-      items.push({:menu => :settings, :label => "#{t('profile.navigation.account_settings')}", :url => links[:settings]})
-    end
-
-    items
+    [{:menu => :home,          :label => "#{t('profile.navigation.home')}",       :url => links[:home]      },
+     {:menu => :stations,      :label => "#{t('profile.navigation.stations')}",   :url => links[:stations]  },
+     # {:menu => :albums,        :label => "#{t('profile.navigation.albums')}",     :url => links[:albums]},
+     {:menu => :activity,      :label => "#{t('profile.navigation.activity')}",   :url => links[:activities]},
+     {:menu => :followers,     :label => "#{t('profile.navigation.followers')}",  :url => links[:followers] }]
   end
-
-
+  
+  def user_nav_links
+    links = {
+      :home     => user_path,
+      :stations => user_stations_path,
+      :activities => activities_path,
+      :followers  => followers_path,
+      :following  => following_index_path
+    }    
+    
+    [{:menu => :home,          :label => "#{t('profile.navigation.home')}",       :url => links[:home]      },
+     {:menu => :stations,      :label => "#{t('profile.navigation.stations')}",   :url => links[:stations]  },
+     {:menu => :activity,      :label => "#{t('profile.navigation.activity')}",   :url => links[:activities]},
+     {:menu => :followers,     :label => "#{t('profile.navigation.followers')}",  :url => links[:followers] },
+     {:menu => :following,     :label => "#{t('profile.navigation.following')}",  :url => links[:following] }]
+  end
+  
+  def my_nav_links
+    links = {
+      :home     => my_dashboard_path,
+      :stations => my_stations_path,
+      :settings => my_settings_path,
+      :activities => my_activities_path,
+      :followers  => my_followers_path,
+      :following  => my_following_index_path
+    }
+    [{:menu => :home,          :label => "#{t('profile.navigation.home')}",        :url => links[:home]      },
+     {:menu => :stations,      :label => "#{t('profile.navigation.stations')}",    :url => links[:stations]  },
+     {:menu => :activity,      :label => "#{t('profile.navigation.activity')}",    :url => links[:activities]},
+     {:menu => :followers,     :label => "#{t('profile.navigation.followers')}",   :url => links[:followers] },
+     {:menu => :following,     :label => "#{t('profile.navigation.following')}",   :url => links[:following] },
+     {:menu => :settings, :label => "#{t('profile.navigation.account_settings')}", :url => links[:settings]}]
+  end
+  
   def user_top_navegation
-    ul_list_to('links', 'current', true)
+    ul_list_to('links', 'current', my_nav_links)
   end
 
   def user_sidebar_links
-    ul_list_to('side_links', 'active', true)
+    ul_list_to('side_links', 'active', user_nav_links)
+  end
+  
+  def artist_sidebar_links
+    ul_list_to('side_links', 'active', artist_nav_links)
   end
 
   def account_sidebar_links
-     ul_list_to('side_links', 'active')
+     ul_list_to('side_links', 'active', my_nav_links)
   end
 
-  def ul_list_to(ul_class, active_class, possessive=nil)
+  def ul_list_to(ul_class, active_class, nav_links)
     items = []
-    nav_links(:possessive => possessive).each do |item|
+    nav_links.each do |item|
       link_classes = li_classes = ""
       link_classes = active_class if @dashboard_menu == item[:menu] and request.request_uri.match(/\/my\//)
       li_classes << "#{item[:menu]} "
