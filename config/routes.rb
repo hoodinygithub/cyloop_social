84ad81db7d26bc489b7697977b8ad67ddbda34fe
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-05-03 05:17:03 -0300

    Widget route and MSN Radio Analytics update

--- config/routes.rb
+++ config/routes.rb
@@ -209,7 +209,7 @@ ActionController::Routing::Routes.draw do |map|
   # Widget
   map.widget 'widget', :controller => 'popups'
   map.resources :popups, :collection => {:widget => :any}
-
+  
   # Keep slug routes at the bottom
   map.user ':slug', :controller => 'accounts', :action => 'show'
   map.user_without_slug ':id', :controller => 'accounts', :action => 'show'
