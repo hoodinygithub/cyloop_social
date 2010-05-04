fb57c1f544207d0ffda29b0ea2744421ef72fa2f
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-05-03 21:00:54 -0300

    Fixes

--- app/controllers/messenger_player/users_controller.rb
+++ app/controllers/messenger_player/users_controller.rb
@@ -1,10 +1,33 @@
 
 class MessengerPlayer::UsersController < MessengerPlayer::PlayerController
+  before_filter :user_status
+  caches_action :status, :cache_path => :users_cache_path.to_proc, :layout => false, :expires_delta => PLAYER_CACHE_EXPIRATION
 
   def status
     respond_to do |format|
       format.xml { render :show }
     end
+  end
+  
+  def user_status
+    if logged_in?
+      'logged_in'
+    elsif session[:msn_live_id]
+      'msn_logged_in'
+    else
+      'anonymous'
+    end
+  end
+  helper_method :user_status
+  
+  def users_cache_path
+    path = "#{CURRENT_SITE.cache_key}/#{params[:controller]}/"
+    unless user_status == 'anonymous'
+      path << current_user.slug unless current_user.nil?
+    else
+      path << "anonymous"
+    end
+    path
   end
 
 end
--- app/views/messenger_player/users/show.xml.builder
+++ app/views/messenger_player/users/show.xml.builder
@@ -1,13 +1,6 @@
 xml.instruct!
-user_status = if logged_in?
-  'logged_in'
-elsif session[:msn_live_id]
-  'msn_logged_in'
-else
-  'anonymous'
-end
 xml.user( :status => user_status ) do
-  if current_user
+  unless current_user.nil?
     [ :id, :slug, :name ].each do |field|
       xml.method_missing( field, current_user.send(field) )
     end
