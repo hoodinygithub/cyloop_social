4dc2ac3637733d611d7f3620d96bc0d19dd50144
Davis Zanetti Cabral <daviscabral@gmail.com>
2010-05-28 15:15:04 -0300

    Total entries to Will Paginate to count again

--- app/controllers/followees_controller.rb
+++ app/controllers/followees_controller.rb
@@ -10,7 +10,7 @@ class FolloweesController < ApplicationController
 
     begin
         sort_types = { :latest => 'followings.approved_at DESC', :alphabetical => 'accounts.name'  }
-        @collection = profile_user.followees.paginate :page => params[:page], :per_page => 12, :order => sort_types[@sort_type]
+        @collection = profile_user.followees.paginate :page => params[:page], :per_page => 12, :order => sort_types[@sort_type], :total_entries => profile_user.followee_count
     rescue NoMethodError
       redirect_to new_session_path
     end
