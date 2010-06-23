d00e82df1fbee3069c8afd92a294dbe5b241d702
Steven Nedlin <snedlin@hoodiny.com>
2010-06-23 12:26:01 -0400

    Fixes to AddIndexesToTopUserStations migration.

--- db/migrate/20100623160710_add_indexes_to_top_user_stations.rb
+++ db/migrate/20100623160710_add_indexes_to_top_user_stations.rb
@@ -1,11 +1,9 @@
 class AddIndexesToTopUserStations < ActiveRecord::Migration
   def self.up
-    ActiveRecord::Base.connection.execute <<-EOF
-    ALTER TABLE top_user_stations 
-    ADD KEY `index_top_user_stations_on_site_id` (site_id, user_station_id),
-    EOF
+    ActiveRecord::Base.connection.execute "ALTER TABLE top_user_stations ADD KEY `index_top_user_stations_on_site_id` (site_id, user_station_id)"
   end
 
   def self.down
+    ActiveRecord::Base.connection.execute "ALTER TABLE top_user_stations DROP KEY `index_top_user_stations_on_site_id`"
   end
 end
