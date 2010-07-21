0a7e1f3d54994eebeed8e74bb80da277f259a2fd
Steven Nedlin <snedlin@hoodiny.com>
2010-07-21 17:24:16 -0400

    Fixed song_buylinks_controller for cases of nil song.

--- app/controllers/song_buylinks_controller.rb
+++ app/controllers/song_buylinks_controller.rb
@@ -1,8 +1,8 @@
 class SongBuylinksController < ApplicationController
 
   def show
-    @song = Song.find(params[:id])
-    @buylinks = @song.buylinks_by_site(current_site)
+    @song = Song.find(params[:id]) rescue nil
+    @buylinks = @song.nil? ? [] : @song.buylinks_by_site(current_site)
     respond_to do |format|
       format.html
       format.xml { render :layout => false }
