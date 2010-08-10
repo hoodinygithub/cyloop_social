eefa98e89b3844b644576b4e0bdaf4393f65578d
Eduardo Molinari <ed.molinari@hotmail.com>
2010-08-10 16:22:02 -0300

    Reviews - Translate Issue

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,15 +1,15 @@
 module ReviewsHelper
   
   def reviews_actions_for(review)
-    read_more = "<a href=\"#\" onclick=\"Base.reviews.show(#{review.id}); return false;\">
-                   #{t('actions.read_more')}
+    view_more = "<a href=\"#\" onclick=\"Base.reviews.show(#{review.id}); return false;\">
+                   #{t('actions.view_more')}
                  </a>" if review.comment.size > 90
 
     edit = "<a href=\"#\" onclick=\"Base.reviews.edit(#{review.id}, true); return false;\">
               #{t('actions.edit')}
             </a>" if review.user == current_user
 
-    actions = [read_more, edit].compact.join('|')
+    actions = [view_more, edit].compact.join('|')
   end
 
 end
