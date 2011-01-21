5c36b915b0e8fde62869ff2b1d2af842a9d9c2f2
David Salazar <dsalazar@hoodiny.com>
2011-01-21 18:11:54 -0500

    Set branch to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,7 +54,7 @@ ssh_options[:paranoid] = false
 # can also specify options that can be used to single out a specific subset of boxes in a
 # particular role, like :primary => true.
 
-set :branch, "logo_swap"
+set :branch, "master"
 
 #EY06 All Sites
 task :production do
