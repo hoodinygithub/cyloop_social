fb8bfb4d95aeb2faa4dfd26d5eaa5e882b70d7d5
Ed Toro <etoro@hoodiny.com>
2011-04-16 00:46:32 -0400

    Fix encryption module so Cyloop doesn't look at the encrypted_ column by default.

--- app/models/account/encryption.rb
+++ app/models/account/encryption.rb
@@ -1,10 +1,14 @@
 module Account::Encryption
   def self.included(base)
     base.class_eval do
-      attr_encrypted :email, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
-      attr_encrypted :name, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
-      attr_encrypted :born_on_string, :key =>'1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
-      attr_encrypted :gender, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
+      # These attributes break references to user.email, user.name, etc. by making them refer to encrypted_email and encrypted_name by default.
+      #attr_encrypted :email, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
+      #attr_encrypted :name, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
+      #attr_encrypted :born_on_string, :key =>'1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
+      #attr_encrypted :gender, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4', :encryptor => MySQLEncryptor, :encode => false
+      def self.encrypt_email(p_email)
+        MySQLEncryptor.encrypt({:value => p_email, :key => '1710d78515ea0f308ed10f5edc0888ee2b893d4def678849b073b703cc678ac4'});
+      end
     end
   end
 
